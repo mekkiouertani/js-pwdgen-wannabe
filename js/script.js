4f29f5454e0c0794b46afc04fb0e633cc6abcd8e
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-06 16:33:13 +0200

    added variables to script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const luckyNumber = 21;
+
+let fname = prompt ('Ciao! Iniziamo col presentarci, qual è il tuo nome?');
+console.log(fname);
+
+let surname = prompt ('Bene, adesso dimmi qual è il tuo cognome');
+console.log(surname);
+
+let color = prompt ('Grande! qual è il tuo colore preferito?');
+console.log(color);
+
